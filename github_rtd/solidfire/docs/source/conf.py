5c75506a10190ba526e4da8aab56bf143351785b
meghakanavi <38721651+meghakanavi@users.noreply.github.com>
2018-05-07 22:43:40 +0530

    Update conf.py

--- github_rtd/solidfire/docs/source/conf.py
+++ github_rtd/solidfire/docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, '/root/github_rtd/solidfire/library/')
+sys.path.insert(0, './library/')
 
 # -- Project information -----------------------------------------------------
 
